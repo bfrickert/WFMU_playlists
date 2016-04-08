9248ba1e2bfa20c707cf12702a5b94fbf99d32ff
Brian Frickert <bfrickert@yahoo.com>
2016-04-07 22:18:58 -0400

    handles anomalies and 'regulars'

--- py/GetPlaylists.py
+++ py/GetPlaylists.py
@@ -5,19 +5,19 @@ import sys
 
 def scrapeKenPlaylists(i, initials):
     url = "http://www.wfmu.org/playlists/%s%s" % (initials,i)
-    try: 
-        response = requests.get(url)
+    response = requests.get(url)
 
-        soup = BeautifulSoup(response.text)
-        playlists = soup.findAll('a')        
-        values = []
-        for playlist in playlists:
-            text = playlist.find(text=True)
-            if text == 'See the playlist' and playlist['href'][-4:] != 'html':
+    soup = BeautifulSoup(response.text)
+    playlists = soup.findAll('a')        
+    values = []
+    for playlist in playlists:
+        text = playlist.find(text=True)
+        try:
+            if '/playlists/shows/' in playlist['href']:
                 values.append("http://www.wfmu.org%s" % playlist['href'])
-        df = pd.DataFrame(values)
-        return df
-    except: pass
+        except: pass
+    df = pd.DataFrame(values)
+    return df
 
 df = pd.DataFrame()
 
@@ -25,5 +25,4 @@ for n in range(2001,2016):
     df = df.append(scrapeKenPlaylists(n, sys.argv[1]))
 
 df = df.append(scrapeKenPlaylists('',sys.argv[1]))
-df.drop_duplicates()
 df.to_csv('data/{0}/playlists.tsv'.format(sys.argv[1]), sep='\t')
